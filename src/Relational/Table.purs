333887e660523f9627c2d887b3bf3dba29b0bd6f
Philip Kamenarsky <p.kamenarsky@gmail.com>
2018-02-08 16:22:09 +0100

    Track query runtime characteristics

--- src/Relational/Table.purs
+++ src/Relational/Table.purs
@@ -3,9 +3,11 @@ module Relational.Table where
 import Data.Symbol
 
 import Data.Map as M
-import Data.Maybe (Maybe)
+import Data.Maybe
 import Data.Record as R
-import Prelude (($), (<>))
+import Data.Traversable
+import Data.Tuple
+import Prelude (($), (<>), flip)
 import Type.Row (class RowLacks, Cons, Nil, kind RowList)
 import Undefined (undefined)
 
@@ -98,3 +100,45 @@ insert_test = insert person persons
 
 lookup_test :: Maybe Person
 lookup_test = lookup (SProxy :: SProxy "age") 5 persons
+
+--------------------------------------------------------------------------------
+
+data Map (o :: Symbol) k v
+data Array' (o :: Symbol) a
+
+data O (o :: Symbol)
+data OMul (o1 :: Type) (o2 :: Type)
+
+data Query (o :: Type) a
+
+lookupO :: ∀ k v o. k -> Map o k v -> Query (O "1") (Maybe v)
+lookupO = undefined
+
+traverseO :: ∀ a b o1 o2. (a -> Query o1 b) -> Array' o2 a -> Query (OMul o1 (O o2)) (Array b)
+traverseO = undefined
+
+elemsO :: ∀ k v o. Map o k v -> Query (O o) (Array' o v)
+elemsO = undefined
+
+mapQ :: ∀ a b o. (a -> b) -> Query o a -> Query o b
+mapQ = undefined
+
+pureQ :: ∀ a. a -> Query (O "1") a
+pureQ = undefined
+
+bindQ :: ∀ a b o1 o2. Query o1 a -> (a -> Query o2 b) -> Query (OMul o1 o2) a
+bindQ = undefined
+
+users :: Map "u" String String 
+users = undefined
+
+addresses :: Map "a" String String 
+addresses = undefined
+
+test = do
+  users'      <- elemsO users
+  addresses'  <- elemsO addresses
+  userAddress <- flip traverseO addresses' \address -> lookupO address users
+  pureQ userAddress
+  where
+    bind = bindQ
